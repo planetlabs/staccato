36b9d51b9891df5fbc63b673a08d325d182c7068
Josh Fix <josh@federal.planet.com>
2019-05-15 16:35:10 -0400

    added point cloud extension

---
+++ staccato-commons/src/main/java/com/planet/staccato/extension/PointCloud.java
@@ -0,0 +1,63 @@
+package com.planet.staccato.extension;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.planet.staccato.elasticsearch.annotation.Mapping;
+import com.planet.staccato.elasticsearch.annotation.MappingType;
+import lombok.Data;
+
+/**
+ * @author joshfix
+ * Created on 2019-05-15
+ */
+public interface PointCloud {
+
+    String EXTENSION_PREFIX = "pc";
+
+    @Mapping(type = MappingType.INTEGER)
+    @JsonProperty(EXTENSION_PREFIX + ":count")
+    Integer getCount();
+    void setCount(Integer count);
+
+    @Mapping(type = MappingType.KEYWORD)
+    @JsonProperty(EXTENSION_PREFIX + ":type")
+    String getType();
+    void setType(String type);
+
+    @Mapping(type = MappingType.KEYWORD)
+    @JsonProperty(EXTENSION_PREFIX + ":encoding")
+    String getEncoding();
+    void setEncoding(String encoding);
+
+    @Mapping(type = MappingType.DOUBLE)
+    @JsonProperty(EXTENSION_PREFIX + ":density")
+    String getDensity();
+    void setDensity(String density);
+
+    @JsonProperty(EXTENSION_PREFIX + ":schema")
+    Schema getSchema();
+    void setSchema(Schema schema);
+
+    @JsonProperty(EXTENSION_PREFIX + ":statistics")
+    Statistics getStatistics();
+    void setStatistics(Statistics statistics);
+
+    @Data
+    class Schema {
+        private String name;
+        private Integer size;
+        private String type;
+    }
+
+    @Data
+    class Statistics {
+        private Double average;
+        private Integer count;
+        private Double maximum;
+        private Double minimum;
+        private String name;
+        private Integer position;
+        private Double stddev;
+        private Double variance;
+    }
+
+}
